27a80b705edb94b76d4c903de150dbe9a3fbed9d
Jamar-Washington <jamarxwashington@gmail.com>
2023-01-11 16:05:03 -0600

    create new tables for new csv files

--- Database/database.sql
+++ Database/database.sql
@@ -12,7 +12,8 @@ CREATE TABLE class_dict (
 CREATE TABLE sports (
 	filepaths VARCHAR(40) NOT NULL,
 	labels VARCHAR(40) NOT NULL,
-	data_set VARCHAR(5) NOT NULL
+	data_set VARCHAR(5) NOT NULL,
+	bins VARCHAR(40) NOT NULL
 );
 
 -- Joining the two databases together
@@ -26,3 +27,24 @@ ON sports.labels = class_dict.sport;
 ALTER TABLE join_table
 	DROP COLUMN class_index,
 	DROP COLUMN labels;
+
+-- Creating tables for mean spending.csv
+CREATE TABLE mean_spending (
+	sport VARCHAR(40) NOT NULL,
+	totalcost curr NOT NULL,
+	registration MONEY(2,1) NOT NULL,
+	equipment MONEY(2,1) NOT NULL,
+	travel MONEY(2,1) NOT NULL,
+	lessons MONEY(2,1) NOT NULL,
+	camps MONEY(2,1) NOT NULL,
+	PRIMARY KEY (sport)
+);
+
+-- Creating tables for avgtime vs avgcost.csv
+CREATE TABLE time_cost (
+	sport VARCHAR(40) NOT NULL,
+	avg_cost MONEY(2,1) NOT NULL,
+	avg_length FLOAT NOT NULL
+);
+
+-- DROP TABLE mean_spending, time_cost;
